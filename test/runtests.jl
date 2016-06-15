79b8e2160b25a8ae4fa05a81f377863694d004d3
dehann <dehann.fourie@gmail.com>
2016-06-15 16:23:43 -0400

    Add plots to testing

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,3 +13,18 @@ unpckd = convert(VariableNodeData, pd)
 @test compare(fg.v[1].attributes["data"], unpckd)
 @test fg.v[1].attributes["data"] == unpckd
 println("Conversions and comparisons agree")
+
+
+
+println("Run plot functions")
+using Gadfly
+# draw all beliefs
+DOYTICKS = false
+xx,ll = ls(fg)
+msgPlots = drawHorBeliefsList(fg, xx, gt=gt,nhor=2);
+evalstr = ""
+for i in 1:length(msgPlots)
+    evalstr = string(evalstr, ",msgPlots[$(i)]")
+end
+pl = eval(parse(string("vstack(",evalstr[2:end],")")));
+@test true
