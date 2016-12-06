441650fc67648f01478b3337d3c0de03a251a428
dehann <dehann.fourie@gmail.com>
2016-12-06 15:05:08 -0500

    moved some of the testing out

--- test/testNumericRootGenericRandomized.jl
+++ test/testNumericRootGenericRandomized.jl
@@ -73,70 +73,70 @@ for i in 1:10
                               convert(Quaternion, so3(y)), tol=1e-8)
 end
 
-type CameraIntrinsic
-  K::Array{Float64,2}
-  CameraIntrinsic(::Void) = new()
-  CameraIntrinsic(;x0=320.0,y0=240.0,fx=510.0,fy=510.0,s=0.0) = new([[fx;s;x0]';[0.0;fy;y0]';[0.0;0;1]'])
-end
-
-# Camera extrinsic must be world in camera frame (cRw)
-type CameraExtrinsic
-  R::SO3
-  t::Vector{Float64}
-  CameraExtrinsic(::Void) = new()
-  CameraExtrinsic(;R=SO3(0),t=zeros(3)) = new(R, t)
-end
-type CameraModelFull
-  ci::CameraIntrinsic
-  ce::CameraExtrinsic
-  # cd::CameraDistortion
-  CameraModelFull(::Void) = new()
-  CameraModelFull(;ci=CameraIntrinsic(), ce=CameraExtrinsic()) = new(ci,ce)
-end
-function project!(ret::Vector{Float64}, ci::CameraIntrinsic, ce::CameraExtrinsic, pt::Vector{Float64})
-  res = ci.K*(ce.R.R*pt + ce.t)
-  ret[1:2] = res[1:2]./res[3]
-  nothing
-end
-project!(ret::Vector{Float64}, cm::CameraModelFull, pt::Vector{Float64}) = project!(ret, cm.ci, cm.ce, pt)
-function project(cm::CameraModelFull, pt::Vector{Float64})
-  res = Vector{Float64}(2)
-  project!(res, cm, pt)
-  return res
-end
-
-# pinhole camera model
-# (x, y)/f = (X, Y)/Z
-function cameraResidual!(
-      res::Vector{Float64},
-      z::Vector{Float64},
-      ci::CameraIntrinsic,
-      ce::CameraExtrinsic,
-      pt::Vector{Float64}  )
-  # in place memory operations
-  project!(res, ci, ce, pt)
-  res[1:2] .*= -1.0
-  res[1:2] += z[1:2]
-  nothing
-end
-
-ci = CameraIntrinsic()
-ce = CameraExtrinsic()
-pt = [1.0;0.0;5.0]
-
-
-gg = (x, res) -> cameraResidual!(res, x, ci, ce, pt)
-# res = zeros(2)
-# @time gg([0.0;0.0], res)
-
-# Profile.clear()
-# @profile
-y = numericRootGenericRandomizedFnc(
-        gg,
-        2, 2, randn(2)  )
+# type CameraIntrinsic
+#   K::Array{Float64,2}
+#   CameraIntrinsic(::Void) = new()
+#   CameraIntrinsic(;x0=320.0,y0=240.0,fx=510.0,fy=510.0,s=0.0) = new([[fx;s;x0]';[0.0;fy;y0]';[0.0;0;1]'])
+# end
 #
-@test abs((ci.K[1,3]+ci.K[1,1]*pt[1]/pt[3]) - y[1]) < 1e-8
-@test abs(y[2] - ci.K[2,3]) < 1e-8
+# # Camera extrinsic must be world in camera frame (cRw)
+# type CameraExtrinsic
+#   R::SO3
+#   t::Vector{Float64}
+#   CameraExtrinsic(::Void) = new()
+#   CameraExtrinsic(;R=SO3(0),t=zeros(3)) = new(R, t)
+# end
+# type CameraModelFull
+#   ci::CameraIntrinsic
+#   ce::CameraExtrinsic
+#   # cd::CameraDistortion
+#   CameraModelFull(::Void) = new()
+#   CameraModelFull(;ci=CameraIntrinsic(), ce=CameraExtrinsic()) = new(ci,ce)
+# end
+# function project!(ret::Vector{Float64}, ci::CameraIntrinsic, ce::CameraExtrinsic, pt::Vector{Float64})
+#   res = ci.K*(ce.R.R*pt + ce.t)
+#   ret[1:2] = res[1:2]./res[3]
+#   nothing
+# end
+# project!(ret::Vector{Float64}, cm::CameraModelFull, pt::Vector{Float64}) = project!(ret, cm.ci, cm.ce, pt)
+# function project(cm::CameraModelFull, pt::Vector{Float64})
+#   res = Vector{Float64}(2)
+#   project!(res, cm, pt)
+#   return res
+# end
+#
+# # pinhole camera model
+# # (x, y)/f = (X, Y)/Z
+# function cameraResidual!(
+#       res::Vector{Float64},
+#       z::Vector{Float64},
+#       ci::CameraIntrinsic,
+#       ce::CameraExtrinsic,
+#       pt::Vector{Float64}  )
+#   # in place memory operations
+#   project!(res, ci, ce, pt)
+#   res[1:2] .*= -1.0
+#   res[1:2] += z[1:2]
+#   nothing
+# end
+#
+# ci = CameraIntrinsic()
+# ce = CameraExtrinsic()
+# pt = [1.0;0.0;5.0]
+#
+#
+# gg = (x, res) -> cameraResidual!(res, x, ci, ce, pt)
+# # res = zeros(2)
+# # @time gg([0.0;0.0], res)
+#
+# # Profile.clear()
+# # @profile
+# y = numericRootGenericRandomizedFnc(
+#         gg,
+#         2, 2, randn(2)  )
+# #
+# @test abs((ci.K[1,3]+ci.K[1,1]*pt[1]/pt[3]) - y[1]) < 1e-8
+# @test abs(y[2] - ci.K[2,3]) < 1e-8
 
 # using ProfileView
 # ProfileView.view()
