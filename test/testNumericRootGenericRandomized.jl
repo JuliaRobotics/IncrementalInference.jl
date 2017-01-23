ed41721a8accd6d4d2b03d739a51296620df1340
dehann <dehann.fourie@gmail.com>
2017-01-23 11:37:56 -0500

    remove TransformUtil test requirement

--- test/testNumericRootGenericRandomized.jl
+++ test/testNumericRootGenericRandomized.jl
@@ -1,5 +1,5 @@
 # test numericRootGenericRandomized
-using IncrementalInference, TransformUtils
+using IncrementalInference
 using Base.Test
 
 # TODO -- resid function definitions are changing
@@ -49,27 +49,27 @@ y = numericRootGenericRandomizedFnc(gg, 1, 1, randn(1))
 
 
 
-println("Increased dimension test")
-
-# 3 dimensional line, z = [a b][x y]' + c
-function rotationresidual!(res::Vector{Float64}, z::Vector{Float64}, var::Tuple)
-  q1 = convert(Quaternion, Euler(z...))
-  q2 = convert(Quaternion, so3(var[2]))
-  qq = q1*q_conj(q2)
-  res[1:3] = vee(convert(so3, qq))
-  nothing
-end
-
-for i in 1:10
-  eul = 0.25*randn(3)
-  gg = (x, res) -> rotationresidual!(res, eul, (zeros(0),x))
-  y = numericRootGenericRandomizedFnc(
-          gg,
-          3, 3, 0.1*randn(3)    )
-  # test the result
-  @test TransformUtils.compare(convert(Quaternion, Euler(eul...)),
-                              convert(Quaternion, so3(y)), tol=1e-8)
-end
+# println("Increased dimension test")
+#
+# # 3 dimensional line, z = [a b][x y]' + c
+# function rotationresidual!(res::Vector{Float64}, z::Vector{Float64}, var::Tuple)
+#   q1 = convert(Quaternion, Euler(z...))
+#   q2 = convert(Quaternion, so3(var[2]))
+#   qq = q1*q_conj(q2)
+#   res[1:3] = vee(convert(so3, qq))
+#   nothing
+# end
+#
+# for i in 1:10
+#   eul = 0.25*randn(3)
+#   gg = (x, res) -> rotationresidual!(res, eul, (zeros(0),x))
+#   y = numericRootGenericRandomizedFnc(
+#           gg,
+#           3, 3, 0.1*randn(3)    )
+#   # test the result
+#   @test TransformUtils.compare(convert(Quaternion, Euler(eul...)),
+#                               convert(Quaternion, so3(y)), tol=1e-8)
+# end
 
 println("Test shuffling function")
 
