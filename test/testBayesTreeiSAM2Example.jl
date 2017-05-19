df4b2130d7a3b55a5c2a859667b4d1d7b917aaff
dehann <dehann.fourie@gmail.com>
2017-05-19 13:58:47 -0400

    wip on more bayes tree testing

---
+++ test/testBayesTreeiSAM2Example.jl
@@ -0,0 +1,82 @@
+using IncrementalInference
+# using KernelDensityEstimate, Gadfly # for vstack
+
+fg = emptyFactorGraph()
+
+N=100
+
+doors = [-100.0;0.0;100.0;300.0]'
+cov = [3.0]
+
+
+v1 = addNode!(fg,:x1,doors,N=N)
+f1  = addFactor!(fg,[v1], Obsv2(doors, cov', [1.0]))
+
+tem = 2.0*randn(1,N)+getVal(v1)+50.0
+v2 = addNode!(fg,:x2, tem, N=N)
+addFactor!(fg,[v1;v2],Odo([50.0]',[2.0]',[1.0]))
+
+v3=addNode!(fg,:x3,4.0*randn(1,N)+getVal(v2)+50.0, N=N)
+addFactor!(fg,[v2;v3],Odo([50.0]',[4.0]',[1.0]))
+
+
+l1=addNode!(fg, :l1, 0.5*randn(1,N)+getVal(v3)+64.0, N=N)
+addFactor!(fg, [:x1,:l1], Ranged([64.0],[0.5],[1.0]))
+addFactor!(fg, [:x2,:l1], Ranged([16.0],[0.5],[1.0]))
+
+l2=addNode!(fg, :l2, 0.5*randn(1,N)+getVal(v3)+64.0, N=N)
+addFactor!(fg, [:x3,:l2], Ranged([16.0],[0.5],[1.0]))
+
+# Graphs.plot(fg.g)
+# writeGraphPdf(fg);
+# run(`evince fg.pdf`)
+
+
+# p = IncrementalInference.getEliminationOrder(fg, ordering=:qr)
+p = [7,10,1,3,5];
+
+println()
+fge = deepcopy(fg)
+println("Building Bayes net...")
+buildBayesNet!(fge, p)
+
+tree = emptyBayesTree()
+buildTree!(tree, fge, p)
+
+# println("Bayes Net")
+# sleep(0.1)
+#fid = open("bn.dot","w+")
+#write(fid,to_dot(fge.bn))
+#close(fid)
+
+# Michael reference -- x2->x1, x2->x3, x2->x4, x2->l1, x4->x3, l1->x3, l1->x4
+println("Bayes Tree")
+if false
+  fid = open("bt.dot","w+")
+  write(fid,Graphs.to_dot(tree.bt))
+  close(fid)
+  run(`dot bt.dot -Tpdf -o bt.pdf`)
+end
+
+# GraphViz.Graph(to_dot(tree.bt))
+#Michael reference 3sig -- x2l1x4x3    x1|x2
+
+import IncrementalInference: buildCliquePotentials
+
+println("Find potential functions for each clique")
+cliq = tree.cliques[1] # start at the root
+buildCliquePotentials(fg, tree, cliq); # fg does not have the marginals as fge does
+
+# now update all factor graph vertices used for this tree
+for v in Graphs.vertices(fg.g)
+  IncrementalInference.dlapi.updatevertex!(fg, v)
+end
+
+
+
+
+# TODO -- add testing to ensure this is the correct tree!
+
+
+
+# run(`evince bt.pdf`)
