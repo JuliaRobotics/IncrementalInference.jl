4a76ea56be8d11daa6bb3b84b4ff9354701f5772
dehann <dehann.fourie@gmail.com>
2018-08-24 07:41:40 -0400

    explicit hypo tests included, wip

--- test/testExplicitMultihypo.jl
+++ test/testExplicitMultihypo.jl
@@ -5,14 +5,15 @@ using Distributions
 using DocStringExtensions
 using Base: Test
 
+using IncrementalInference
 
-@testset "test assembleHypothesesElements! without multihypothesis..." begin
+@testset "test IncrementalInference.assembleHypothesesElements! without multihypothesis..." begin
 
 s2_1_gt1 = 1:2
 s2_1_gt2 = (1:20,Int[])
 s2_1_gt3 = [(1,1:2); (2,Int[])]
 s2_1_gt4 = Int[]
-s2_1 = assembleHypothesesElements!(nothing, 20, 1, 2 )
+s2_1 = IncrementalInference.assembleHypothesesElements!(nothing, 20, 1, 2 )
 @test sum(s2_1_gt1 .- s2_1[1]) == 0
 @test sum(s2_1_gt2[1] .- s2_1[2][1]) == 0
 @test sum(s2_1_gt2[2] .- s2_1[2][2]) == 0
@@ -28,7 +29,7 @@ s2_2_gt2 = (1:20,Int[])
 s2_2_gt3 = [(1,1:2); (2,Int[])]
 s2_2_gt4 = Int[]
 
-s2_2 = assembleHypothesesElements!(nothing, 20, 2, 2 )
+s2_2 = IncrementalInference.assembleHypothesesElements!(nothing, 20, 2, 2 )
 
 @test sum(s2_2_gt1 .- s2_2[1]) == 0
 @test sum(s2_2_gt2[1] .- s2_2[2][1]) == 0
@@ -43,14 +44,14 @@ end
 
 
 
-@testset "test assembleHypothesesElements! with bimodality..." begin
+@testset "test IncrementalInference.assembleHypothesesElements! with bimodality..." begin
 
 s3_1_gt1 = [1]
 s3_1_gt2 = (0,5,5,20)
 s3_1_gt3 = [(1,Int[]); (2,Int[1;2]); (3,Int[1;3])]
 s3_1_gt4 = 20
 
-s3_1 = assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 1, 3 )
+s3_1 = IncrementalInference.assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 1, 3 )
 
 @test sum(s3_1_gt1 - s3_1[1]) == 0
 @test sum(s3_1_gt2[1] .- s3_1[2][1]) == 0
@@ -72,7 +73,7 @@ s3_1 = assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 1, 3 )
 @test length(s3_1[4]) == s3_1_gt4
 
 
-s3_2 = assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 2, 3 )
+s3_2 = IncrementalInference.assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 2, 3 )
 
 
 
@@ -80,14 +81,14 @@ s3_2 = assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 2, 3 )
 
 
 
-s3_3 = assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 3, 3 )
+s3_3 = IncrementalInference.assembleHypothesesElements!(Categorical([0.0;0.5;0.5]), 20, 3, 3 )
 
 
 
 
 
 
-s4_2 = assembleHypothesesElements!(Categorical([0.0;0.3;0.3;0.4]), 20, 2, 4 )
+s4_2 = IncrementalInference.assembleHypothesesElements!(Categorical([0.0;0.3;0.3;0.4]), 20, 2, 4 )
 
 
 end
