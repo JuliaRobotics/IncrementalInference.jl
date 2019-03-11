83077935411c41e3ca11d79c365751a8c2e2357d
dehann <dehann.fourie@gmail.com>
2019-03-10 22:15:48 -0400

    remove duplicate docstrings

--- src/FactorGraph01.jl
+++ src/FactorGraph01.jl
@@ -693,12 +693,6 @@ function addFactor!(fgl::FactorGraph,
 
   return newvert
 end
-
-"""
-    $(SIGNATURES)
-
-Add factor with user defined type <: FunctorInferenceType to the factor graph object.  Define whether the automatic initialization of variables should be performed.  Use order sensitive `multihypo` keyword argument to define if any variables are related to data association uncertainty.
-"""
 function addFactor!(
       fgl::FactorGraph,
       xisyms::Vector{Symbol},
