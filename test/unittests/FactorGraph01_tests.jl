ab626adc2d7f9e81d7b804db4d06d146a6e66b0a
gearsad <sclaassens.ad@gmail.com>
2019-06-02 20:07:00 -0500

    Updated end-to-end test

--- test/unittests/FactorGraph01_tests.jl
+++ test/unittests/FactorGraph01_tests.jl
@@ -36,7 +36,7 @@ elimOrder = getEliminationOrder(dfgPrime)
 # Forcing elimination order to match the documentation
 elimOrder = [:l1, :l2, :x1, :x2, :x3]
 
-# tree for elim order  [:l1, :l2, :x1, :x2, :x3]
+# tree for elim order: [:l1, :l2, :x1, :x2, :x3]
 # :l1 <-- :x1, :x2
 # :x1 <-- :x2
 # :x2 <-- :x3
@@ -67,9 +67,50 @@ tree = wipeBuildNewTree!(dfg, drawpdf=true, show=false)
 # drawTree(tree, show=true)
 
 
+# TODO: Test
 # Upward solve steps with clique state machine
-@info "Complete upward solve..."
-smtasks, ch = initInferTreeUp!(dfg, tree, drawtree=true, recordcliqs=[:x1;:x2;:x3] )
+# @info "Complete upward solve..."
+# smtasks, ch = initInferTreeUp!(dfg, tree, drawtree=true, recordcliqs=[:x1;:x2;:x3] )
+
+
+
+
+
+
+## Upward solve steps with clique state machine
+# @info "Complete upward solve..."
+# smtasks, ch = initInferTreeUp!(fgl, bt, N=N, drawtree=drawpdf, recordcliqs=recordcliqs, limititers=limititers, skipcliqids=skipcliqids )
+
+# @info "solve leaf clique with single state machine"
+# alltasks[i] = @async tryCliqStateMachineSolve!(fgl, treel, i, cliqHistories, drawtree=drawtree, N=N, limititers=limititers, recordcliqs=recordcliqs)
+# manually
+
+resetTreeCliquesForUpSolve!(tree)
+setTreeCliquesMarginalized!(dfg, tree)
+
+
+
+cliq = whichCliq(tree, :x1)
+
+
+
+# syms = getCliqAllVarSyms(dfg, csmc.cliq)
+# cliqSubFg = buildSubgraphFromLabels(dfg, syms)
+
+# function buildSubgraphFromLabels(dfg::G, syms::Vector{Symbol}) where G <: AbstractDFG
+#   cliqSubFg = initfg()
+#   for sym in syms
+#     DFG.GraphsJl.getSubgraphAroundNode(dfg, DFG.GraphsJl.getVariable(dfg, sym), 2, false, cliqSubFg)
+#   end
+#   return cliqSubFg
+# end
+
+
+
+
+# history = cliqInitSolveUpByStateMachine!(dfg, tree, cliq,drawtree=true,
+                                         # limititers=50, recordhistory=true )
+0
 
 
 
@@ -77,11 +118,101 @@ smtasks, ch = initInferTreeUp!(dfg, tree, drawtree=true, recordcliqs=[:x1;:x2;:x
 
 
 
-## See results
 
-using RoMEPlotting
+children = Graphs.ExVertex[]
+for ch in Graphs.out_neighbors(cliq, tree.bt)
+  push!(children, ch)
+end
+prnt = getParent(tree, cliq)
+csmc = CliqStateMachineContainer(dfg, initfg(), tree, cliq, prnt, children, false, true, true)
+statemachine = StateMachine{CliqStateMachineContainer}(next=isCliqUpSolved_StateMachine)
 
-drawPosesLandm(dfg)
+
+limititers = 100
+recordhistory = true
+
+
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+
+
+
+
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+
+
+
+csmc.cliqSubFg.nodeCounter
+
+
+
+writeGraphPdf(csmc.cliqSubFg, show=true, engine="neato")
+
+
+
+
+addFactor!(dfg, [:x1, :x2], pp, autoinit=false)
+DFG.GraphsJl.lsf(dfg, :x1x2f1)
+
+
+0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+statemachine(csmc, verbose=true, iterlimit=limititers, recordhistory=recordhistory)
+
+
+
+
+
+
+
+
+
+
+
+DFG.GraphsJl.getFactorIds(dfg)
+
+
+
+
+cliqHistories = Dict{Int,Vector{Tuple{DateTime, Int, Function, CliqStateMachineContainer}}}()
+isTreeSolved(treel, skipinitialized=true)
+
+
+
+# TODO: Nary tests for factors....
+
+
+
+DFG.GraphsJl.getFactor(dfg, :x1x2f1)
+
 
 
 ## See picture of upward clique association matrix
