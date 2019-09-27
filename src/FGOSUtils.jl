2b5df3a223bc42ed6eb965f2317b4667a18255c2
dehann <dehann.fourie@gmail.com>
2019-09-27 12:05:39 -0400

    fix test add sphere1

--- src/FGOSUtils.jl
+++ src/FGOSUtils.jl
@@ -3,6 +3,11 @@
 # of convert in their namespace
 
 
+
+manikde!(pts::AbstractArray{Float64,2}, vartype::InferenceVariable) = manikde!(pts, getManifolds(vartype))
+manikde!(pts::AbstractArray{Float64,2}, vartype::Type{InferenceVariable}) = manikde!(pts, getManifolds(vartype))
+
+
 function getMeasurements(dfg::G, fsym::Symbol, N::Int=100) where G <: AbstractDFG
   fnc = getFactorFunction(dfg, fsym)
   getSample(fnc, N)
