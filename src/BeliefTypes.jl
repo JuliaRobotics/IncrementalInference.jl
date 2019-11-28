665ad4383f0ca9661d0cc9ebfdb0cc2bd70e78ef
Johannes Terblanche <terblanche.j@gmail.com>
2019-11-28 17:38:25 +0200

    Idea for consolidate message priors #459

--- src/BeliefTypes.jl
+++ src/BeliefTypes.jl
@@ -42,15 +42,40 @@ end
 
 ### SOME CONVERGENCE REQUIRED ^^^
 
+#DEV NOTE it looks like it can be consolidated into one type
+# if we can pass messages similar to EasyMessage:
+# pts::Array{Float64,2}
+# bws::Array{Float64,1}
+# option a tuple
+# bellief::Dict{Symbol, NamedTuple{(:vec, :bw, :inferdim),Tuple{Array{Int64,1},Array{Int64,1},Float64}}}
+# or an extra type
+# or the MsgPrior/PackedMessagePrior, depending on the serialization requirement of the channel
+# but I would think only one message type
+
+# mutable struct NBPMessage <: Singleton
+#   status::Symbol # Ek kort die in die boodskap
+#   p::Dict{Symbol, EasyMessage}
+# end
+
+struct TreeBelief
+  val::Array{Float64,2}
+  bw::Array{Float64,2}
+  inferdim::Float64
+  # manifolds::T TODO ? JT not needed as you have the variable with all the info in it?
+end
+TreeBelief(p::BallTreeDensity, inferdim::Real=0.0) = TreeBelief(getPoints(p), getBW(p), inferdim)
+TreeBelief(val::Array{Float64,2}, bw::Array{Float64,2}, inferdim::Real=0.0) = TreeBelief(val, bw, inferdim)
+
 abstract type AbstractBeliefMessage end
 #JT Ek maak nog whahahaha, maar kom ons inheret van AbstractBeliefMessage?
 # of ons gebruik T parameter in belief::Dict{Symbol, T} where T <: Union{BallTreeDensity, Vector{Float64}}
 struct ParametricBeliefMessage <: AbstractBeliefMessage
   # TODO JT nog velde, maar dis wat ek sover aan kan dink
-  status::Symbol #ek voel mens kan dit dalk nodig he
-  belief::Dict{Symbol, Vector{Float64}}#TODO Marix
-  inferdim::Dict{Symbol, Float64} #dink dis die ene wat Dehann soek
+  status::Symbol
+  belief::Dict{Symbol, TreeBelief}
+  # of dalk named tuple maak
+  # bellief::Dict{Symbol, NamedTuple{(:val, :bw, :inferdim),Tuple{Array{Int64,1},Array{Int64,1},Float64}}}
 end
 
 ParametricBeliefMessage(status::Symbol) =
-        ParametricBeliefMessage(status, Dict{Symbol, Vector{Float64}}(),Dict{Symbol, Float64}())
+        ParametricBeliefMessage(status, Dict{Symbol, Dict{Symbol, TreeBelief}())
