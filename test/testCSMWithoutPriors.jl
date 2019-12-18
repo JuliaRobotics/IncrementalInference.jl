c8cc20c88ffd9c40f6287a01d31e45ed182b4714
dehann <dehann.fourie@gmail.com>
2019-12-17 21:33:55 -0500

    new csm test, no prior,
    cc @GearsAD -- #485, thanks!

---
+++ test/testCSMWithoutPriors.jl
@@ -0,0 +1,34 @@
+# IIF #485 --
+
+using Test
+using Logging
+using DistributedFactorGraphs
+using IncrementalInference
+
+
+
+logger = SimpleLogger(stdout, Logging.Debug)
+global_logger(logger)
+dfg = initfg() #LightDFG{SolverParams}(solverParams=SolverParams())
+# Add some nodes.
+v1 = addVariable!(dfg, :a, ContinuousScalar, labels = [:POSE])
+v2 = addVariable!(dfg, :b, ContinuousScalar, labels = [:POSE])
+v3 = addVariable!(dfg, :c, ContinuousScalar, labels = [:LANDMARK])
+f1 = addFactor!(dfg, [:a; :b], LinearConditional(Normal(50.0,2.0)) )
+f2 = addFactor!(dfg, [:b; :c], LinearConditional(Normal(50.0,2.0)) )
+
+drawGraph(dfg, show=true)
+
+getSolverParams(dfg).limititers = 50
+getSolverParams(dfg).dbg = true
+getSolverParams(dfg).dbg = true
+getSolverParams(dfg).drawtree = true
+getSolverParams(dfg).showtree = true
+
+
+tree, smtasks, hist = solveTree!(dfg, recordcliqs=ls(dfg))
+
+
+
+
+#
