908da7c4ad2ad2f0587d4323b7cdbee840d93964
dehann <dehann.fourie@gmail.com>
2020-01-05 14:00:40 -0500

    also check old factors for ex1

--- test/testCliqueFactors.jl
+++ test/testCliqueFactors.jl
@@ -35,11 +35,8 @@ addFactor!(fg, [:x2;:l1], lc, autoinit=false)
 addFactor!(fg, [:x0;], lp, autoinit=false)
 addFactor!(fg, [:l0;], lp, autoinit=false)
 
+# build tree
 vo = Symbol[:x2, :x0, :l0, :x3, :x1, :l1, :x4]
-# getSolverParams(fg).dbg = true
-# tree, smt, hist = solveTree!(fg, variableOrder=vo)
-
-
 tree = resetBuildTreeFromOrder!(fg, vo)
 # drawTree(tree, show=true)
 
@@ -59,31 +56,38 @@ lsvars = ls(fg)
 
 ## Now check if factors in cliques are okay
 
-C3_fg = buildCliqSubgraph(fg, tree, getCliq(tree, :x0) )
+C3 = getCliq(tree, :x0)
+C3_fg = buildCliqSubgraph(fg, tree, C3 )
 # drawGraph(C3_fg, show=true)
 
 C3_fcts = [:x0l0f1;:x0l1f1;:x0x1f1;:x0f1]
 
 @test intersect(ls(C3_fg), [:x0; :x1; :l0; :l1]) |> length == 4
 @test intersect(lsf(C3_fg), C3_fcts) |> length == length(C3_fcts)
+@test intersect(getCliqFactorIdsAll(C3), C3_fcts) |> length == length(C3_fcts)
 
 
-C2_fg = buildCliqSubgraph(fg, tree, getCliq(tree, :l0) )
+C2 = getCliq(tree, :l0)
+C2_fg = buildCliqSubgraph(fg, tree, C2)
 # drawGraph(C2_fg, show=true)
 
 C2_fcts = [:x1x2f1; :x2x3f1; :x2l0f1; :x2l1f1; :l0f1]
 
 @test intersect(ls(C2_fg), [:x3; :x2; :x1; :l0; :l1]) |> length == 5
 @test intersect(lsf(C2_fg), C2_fcts) |> length == length(C2_fcts)
+@test intersect(getCliqFactorIdsAll(C2), C2_fcts) |> length == length(C2_fcts)
 
 
-C1_fg = buildCliqSubgraph(fg, tree, getCliq(tree, :x4) )
+C1 = getCliq(tree, :x4)
+C1_fg = buildCliqSubgraph(fg, tree, C1)
 # drawGraph(C1_fg, show=true)
 
 C1_fcts = [:x3x4f1;]
 
 @test intersect(ls(C1_fg), [:x3; :x4; :x1; :l1]) |> length == 4
 @test intersect(lsf(C1_fg), C1_fcts) |> length == length(C1_fcts)
+@test intersect(getCliqFactorIdsAll(C1), C1_fcts) |> length == length(C1_fcts)
+
 
 # check that all factors are counted
 allCliqFcts = union(C1_fcts, C2_fcts, C3_fcts)
