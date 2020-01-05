d81cb06dcec1110a86cee95f4e13a0b9a1e96578
dehann <dehann.fourie@gmail.com>
2020-01-05 16:01:06 -0500

    test for orphaned FGs

--- test/runtests.jl
+++ test/runtests.jl
@@ -58,6 +58,8 @@ include("testBasicGraphs.jl")
     include("testlocalconstraintexamples.jl")
 end
 
+# include("testSolveOrphanedFG.jl")
+
 # include("priorusetest.jl")
 
 include("testExplicitMultihypo.jl")
---
+++ test/testSolveOrphanedFG.jl
@@ -0,0 +1,28 @@
+# test forest of graphs can solve with CSM,
+
+using IncrementalInference
+using Test
+
+
+
+@testset "Test forest of orphaned graphs"
+
+fg = initfg()
+addVariable!(fg, :x0, ContinuousScalar)
+addFactor!(fg, [:x0;], Prior(Normal()))
+addVariable!(fg, :x1, ContinuousScalar)
+addFactor!(fg, [:x0;:x1], LinearConditional(Normal()))
+
+fg = initfg()
+addVariable!(fg, :x10, ContinuousScalar)
+addFactor!(fg, [:x10;], Prior(Normal()))
+addVariable!(fg, :x11, ContinuousScalar)
+addFactor!(fg, [:x10;:x11], LinearConditional(Normal()))
+
+# solve factor graph with two orphaned components
+tree, smt, hist = solveTree!(fg)
+
+# test tree will have two different root nodes
+# ...
+
+end
