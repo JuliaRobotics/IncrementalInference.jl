3cf80e6a37e6a48012087577cb1d0e95e87205cc
dehann <dehann.fourie@gmail.com>
2020-02-25 18:31:26 -0500

    deprecated api fix

--- test/testpartialconstraint.jl
+++ test/testpartialconstraint.jl
@@ -189,10 +189,10 @@ val, = predictbelief(fg, v2, [f3;f4], N=N)
 @test (Statistics.std(val,dims=2)[2] .- 1.0) < 3.0
 
 
-tree = wipeBuildNewTree!(fg )#, drawpdf=true)
-# run(`evince bt.pdf`)
 
-inferOverTree!(fg,tree, N=N)
+getSolverParams(fg).N = N
+tree, smt, hist = solveTree!(fg)
+
 
 pts = getVal(fg, :x1)
 @test norm(Statistics.mean(pts,dims=2)[1] .- [0.0]) < 0.5
