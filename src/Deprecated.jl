1cedbf9dbf0660733d2a1c192a984dad3b1ebc68
dehann <dehann.fourie@gmail.com>
2020-03-19 01:25:11 -0400

    deprecate writeGraphPdf

--- src/Deprecated.jl
+++ src/Deprecated.jl
@@ -28,51 +28,32 @@
 # end
 
 
+# function writeGraphPdf(fgl::G;
+#                        viewerapp::String="evince",
+#                        filepath::AS="/tmp/fg.pdf",
+#                        engine::AS="neato", #sfdp
+#                        show::Bool=true ) where {G <: AbstractDFG, AS <: AbstractString}
+#   #
+#   @warn "writeGraphPdf is function changing to drawGraph, see DFG.toDotFile(dfg) as part of the long term solution."
+#
+#   fgd = fgl
+#   @info "Writing factor graph file"
+#   fext = split(filepath, '.')[end]
+#   fpwoext = filepath[1:(end-length(fext)-1)] # split(filepath, '.')[end-1]
+#   dotfile = fpwoext*".dot"
+#
+#   # create the dot file
+#   DFG.toDotFile(fgl, dotfile)
+#
+#   try
+#     run(`$(engine) $(dotfile) -T$(fext) -o $(filepath)`)
+#     show ? (@async run(`$(viewerapp) $(filepath)`)) : nothing
+#   catch e
+#     @warn "not able to show $(filepath) with viewerapp=$(viewerapp). Exception e=$(e)"
+#   end
+#   nothing
+# end
 
-"""
-    $SIGNATURES
-
-Draw and show the factor graph `<:AbstractDFG` via system graphviz and pdf app.
-
-Notes
-- Should not be calling outside programs.
-- Need long term solution
-- DFG's `toDotFile` a better solution -- view with `xdot` application.
-- also try `engine={"sfdp","fdp","dot","twopi","circo","neato"}`
-
-Future:
-- Might be kept with different call strategy since this function so VERY useful!
-- Major issue that this function calls an external program such as "evince", which should be
-   under user control only.
-- Maybe solution is
-- ```toDot(fg,file=...); @async run(`xdot file.dot`)```, or
-  - ```toDot(fg,file=...); exportPdf(...); @async run(`evince ...pdf`)```.
-"""
-function writeGraphPdf(fgl::G;
-                       viewerapp::String="evince",
-                       filepath::AS="/tmp/fg.pdf",
-                       engine::AS="neato", #sfdp
-                       show::Bool=true ) where {G <: AbstractDFG, AS <: AbstractString}
-  #
-  @warn "writeGraphPdf is function changing to drawGraph, see DFG.toDotFile(dfg) as part of the long term solution."
-
-  fgd = fgl
-  @info "Writing factor graph file"
-  fext = split(filepath, '.')[end]
-  fpwoext = filepath[1:(end-length(fext)-1)] # split(filepath, '.')[end-1]
-  dotfile = fpwoext*".dot"
-
-  # create the dot file
-  DFG.toDotFile(fgl, dotfile)
-
-  try
-    run(`$(engine) $(dotfile) -T$(fext) -o $(filepath)`)
-    show ? (@async run(`$(viewerapp) $(filepath)`)) : nothing
-  catch e
-    @warn "not able to show $(filepath) with viewerapp=$(viewerapp). Exception e=$(e)"
-  end
-  nothing
-end
 
 
 
@@ -99,6 +80,20 @@ end
 ## Delete in v0.11
 ##==============================================================================
 
+"""
+    $SIGNATURES
+
+writeGraphPdf deprecated, use drawGraph instead
+"""
+function writeGraphPdf(fgl::AbstractDFG;
+                       viewerapp::AbstractString="evince",
+                       filepath::AbstractString="/tmp/fg.pdf",
+                       engine::AbstractString="neato",
+                       show::Bool=true )
+  #
+  @warn "writeGraphPdf deprecated, use drawGraph instead"
+  drawGraph(fgl, viewerapp=viewerapp, filepath=filepath, engine=engine, show=show )
+end
 
 @deprecate manualinit!(dfg::AbstractDFG, vert::DFGVariable, pX::BallTreeDensity) initManual!(dfg::AbstractDFG, vert::DFGVariable, pX::BallTreeDensity)
 @deprecate manualinit!(dfg::AbstractDFG, sym::Symbol, pX::BallTreeDensity) initManual!(dfg::AbstractDFG, sym::Symbol, pX::BallTreeDensity) false
