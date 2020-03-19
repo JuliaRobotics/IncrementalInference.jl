8681e2b1ba864af5b3af624a7da657f74e5ed0f3
dehann <dehann.fourie@gmail.com>
2020-03-19 07:17:27 -0400

    remove commented code

--- src/BeliefTypes.jl
+++ src/BeliefTypes.jl
@@ -1,8 +1,6 @@
 
 export TreeBelief, LikelihoodMessage
 
-# const TreeBelief{T} = EasyMessage{T}
-# const EasyMessage{T} = TreeBelief{T}
 
 """
     $TYPEDEF
@@ -85,26 +83,6 @@ const BeliefMessage = LikelihoodMessage
 
 
 ### EVERYTHING BELOW IS/SHOULD BE DEPRECATED
-
-
-
-# """
-# $(TYPEDEF)
-#
-# Condensed representation of KernelDensityEstimate, by saving points and bandwidth
-# """
-# mutable struct EasyMessage{T <: Tuple}
-#   val::Array{Float64,2}
-#   bw::Array{Float64,1}
-#   manifolds::T
-#   inferdim::Float64
-#   EasyMessage{T}() where {T <: Tuple} = new{T}()
-#   EasyMessage{T}(a::Array{Float64,2}, b::Array{Float64,1}, manis::T, inferdim::Union{Float64, Int32, Int64}=0.0) where {T <: Tuple} = new{T}(a,b, manis, Float64(inferdim))
-#   EasyMessage{T}(p::BallTreeDensity, manis::T, inferdim::Union{Float64, Int32, Int64}=0.0) where {T <: Tuple}  = new{T}(getPoints(p), getBW(p)[:,1], manis, Float64(inferdim))
-# end
-#
-# EasyMessage(a::Array{Float64,2}, b::Array{Float64,1}, manis::T, inferdim::Union{Float64, Int32, Int64}=0) where {T <: Tuple} = EasyMessage{T}(a, b, manis, inferdim)
-# EasyMessage(p::BallTreeDensity, manis::T, inferdim::Union{Float64, Int32, Int64}=0) where {T <: Tuple} = EasyMessage{T}(p, manis, inferdim)
 
 
 
