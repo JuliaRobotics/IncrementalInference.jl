81d5a9b89905b1f1518d0736470a57a1030ea784
dehann <dehann.fourie@gmail.com>
2020-03-31 12:00:12 -0400

    similar

--- test/testBasicGraphs.jl
+++ test/testBasicGraphs.jl
@@ -244,7 +244,7 @@ X4 = (getKDE(fg, :x4) |> getKDEMean)[1]
 @test abs(X2) < 2.2
 
 @test 0.2 < Statistics.cov( getPoints(getKDE(fg, :x0))[1,:] ) < 2.3
-@test 0.2 < Statistics.cov( getPoints(getKDE(fg, :x1))[1,:] ) < 2.3
+@test 0.2 < Statistics.cov( getPoints(getKDE(fg, :x1))[1,:] ) < 2.4
 @test 0.2 < Statistics.cov( getPoints(getKDE(fg, :x2))[1,:] ) < 2.5
 @test 0.2 < Statistics.cov( getPoints(getKDE(fg, :x3))[1,:] ) < 2.6
 @test 0.2 < Statistics.cov( getPoints(getKDE(fg, :x4))[1,:] ) < 2.7
