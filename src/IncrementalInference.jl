334dac0e6df82d287beffa957832526c31c7b35d
dehann <dehann.fourie@gmail.com>
2020-05-26 02:06:15 -0400

    rempve old import

--- src/IncrementalInference.jl
+++ src/IncrementalInference.jl
@@ -46,7 +46,7 @@ import Distributions: sample
 import Random: rand, rand!
 import KernelDensityEstimate: getBW
 import ApproxManifoldProducts: kde!, manikde!
-import DistributedFactorGraphs: addVariable!, addFactor!, ls, lsf, isInitialized, hasOrphans, compare, compareAllSpecial
+import DistributedFactorGraphs: addVariable!, addFactor!, ls, lsf, isInitialized, compare, compareAllSpecial
 import DistributedFactorGraphs: rebuildFactorMetadata!
 
 # will be deprecated in IIF
