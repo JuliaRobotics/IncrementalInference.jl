77e8b24b37c0c37ea9ba227341c32b77b4645e72
Johannes Terblanche <terblanche.j@gmail.com>
2020-10-02 10:24:51 +0200

    canonical LinearConditional->LinearRelative

--- src/CanonicalGraphExamples.jl
+++ src/CanonicalGraphExamples.jl
@@ -17,17 +17,17 @@ function generateCanonicalFG_Kaess(;graphinit::Bool=false)
 
 
   addVariable!(fg,:x2, ContinuousScalar)
-  addFactor!(fg,[:x1, :x2], LinearConditional(Normal()), graphinit=graphinit)
+  addFactor!(fg,[:x1, :x2], LinearRelative(Normal()), graphinit=graphinit)
 
   addVariable!(fg, :x3, ContinuousScalar)
-  addFactor!(fg,[:x2,:x3],LinearConditional(Normal()), graphinit=graphinit)
+  addFactor!(fg,[:x2,:x3],LinearRelative(Normal()), graphinit=graphinit)
 
   addVariable!(fg, :l1, ContinuousScalar)
-  addFactor!(fg, [:x1,:l1], LinearConditional(Normal()) , graphinit=graphinit)
-  addFactor!(fg, [:x2,:l1], LinearConditional(Normal()) , graphinit=graphinit)
+  addFactor!(fg, [:x1,:l1], LinearRelative(Normal()) , graphinit=graphinit)
+  addFactor!(fg, [:x2,:l1], LinearRelative(Normal()) , graphinit=graphinit)
 
   addVariable!(fg, :l2, ContinuousScalar)
-  addFactor!(fg, [:x3,:l2], LinearConditional(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x3,:l2], LinearRelative(Normal()), graphinit=graphinit)
 
   return fg
 end
@@ -53,16 +53,16 @@ function generateCanonicalFG_TestSymbolic(;graphinit::Bool=false)
   addVariable!(fg, :l2, ContinuousScalar)
   addVariable!(fg, :l3, ContinuousScalar)
 
-  addFactor!(fg, [:x1;:l1], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x1;:x2], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x2;:l1], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x2;:x3], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x3;:x4], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x4;:l2], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x4;:x5], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:l2;:x5], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x4;:l3], LinearConditional(Normal()), graphinit=graphinit)
-  addFactor!(fg, [:x5;:l3], LinearConditional(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x1;:l1], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x1;:x2], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x2;:l1], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x2;:x3], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x3;:x4], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x4;:l2], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x4;:x5], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:l2;:x5], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x4;:l3], LinearRelative(Normal()), graphinit=graphinit)
+  addFactor!(fg, [:x5;:l3], LinearRelative(Normal()), graphinit=graphinit)
 
   return fg
 end
@@ -90,16 +90,16 @@ function generateCanonicalFG_CaesarRing1D(;graphinit::Bool=false)
     addVariable!(fg, :x6, ContinuousScalar)
 
     addFactor!(fg, [:x0], Prior(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x0;:x1], LinearConditional(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x1;:x2], LinearConditional(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x2;:x3], LinearConditional(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x3;:x4], LinearConditional(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x4;:x5], LinearConditional(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x5;:x6], LinearConditional(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x0;:x1], LinearRelative(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x1;:x2], LinearRelative(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x2;:x3], LinearRelative(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x3;:x4], LinearRelative(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x4;:x5], LinearRelative(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x5;:x6], LinearRelative(Normal()), graphinit=graphinit)
 
     addVariable!(fg, :l1, ContinuousScalar)
-    addFactor!(fg, [:x0;:l1], LinearConditional(Normal()), graphinit=graphinit)
-    addFactor!(fg, [:x6;:l1], LinearConditional(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x0;:l1], LinearRelative(Normal()), graphinit=graphinit)
+    addFactor!(fg, [:x6;:l1], LinearRelative(Normal()), graphinit=graphinit)
 
     return fg
 end
