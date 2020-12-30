920a24395e57792af52a33bb7ab4c890b93f1c3b
dehann <dehann.fourie@gmail.com>
2020-12-30 11:59:02 -0500

    see #1083

--- src/JunctionTree.jl
+++ src/JunctionTree.jl
@@ -626,6 +626,7 @@ Build Bayes/Junction/Elimination tree from a given variable ordering.
 
 DevNotes
 - TODO use `solvable` filter during local graph copy step
+- TODO review `buildCliquePotentials` and rather incorporate into CSM, see #1083
 
 Related
 
