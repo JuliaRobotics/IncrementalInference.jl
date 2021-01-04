2541a4806ce49ece028de9a6a744962b1892007b
dehann <dehann.fourie@gmail.com>
2021-01-04 17:46:45 -0500

    update old ccw test

--- test/testCommonConvWrapper.jl
+++ test/testCommonConvWrapper.jl
@@ -36,6 +36,8 @@ mutable struct FunctorArray{T}
   a::Array{T, 1}
 end
 
+##
+
 @testset "FunctorArray" begin
 
 function testarray!(a1::Array{Float64, 2}, a2::Array{Float64,2})
@@ -63,86 +65,17 @@ fvar([0.0])
 end
 
 
-struct Test2 <: FunctorInferenceType
-  dummy::Int
-end
-
-getSample(::Test2, N::Int=1) = (reshape(randn(N),1,N), )
-
-function (tt::Test2)( res::AbstractVector{<:Real},
-                      userdata::FactorMetadata,
-                      idx::Int,
-                      meas::Tuple,
-                      tp1::AbstractArray{<:Real,2},
-                      tp2::AbstractArray{<:Real,2}  )
-  #
-  tp1[1,1]=-2.0;
-  res[:] .= 1.0
-  nothing;
-end
-
-##
-
-@testset "CommonConvWrapper test" begin
-
-##
-
-N = 3
-tst2 = Test2(1)
-A = rand(2,N)
-B = rand(2,N)
-At = deepcopy(A)
-t = Array{Array{Float64,2},1}()
-push!(t,A)
-push!(t,B)
-t[1][1,1] = -10.0
-@test A[1,1] == -10
-
-##
-
-fg = initfg()
-X0 = addVariable!(fg, :x0, ContinuousEuclid{1})
-X1 = addVariable!(fg, :x1, ContinuousEuclid{1})
-
-addFactor!(fg, [:x0;:x1], tst2, graphinit=false)
-
-fmd = FactorMetadata([X0;X1], [:x0; :x1],t, :null, nothing)
-
-##
-
-ccw = CommonConvWrapper(tst2, t[1], 2, t, fmd) # generalwrapper.measurement = rand(1,1)
-
-##
-
-ccw.cpt[Threads.threadid()].factormetadata
-ccw.cpt[Threads.threadid()].particleidx
-ccw.measurement = (randn(1,N),)
-ccw.params[ccw.cpt[Threads.threadid()].activehypo]
-
-x, res = zeros(2), zeros(2)
-@time ccw(res, x)
-
-
-At[1,1] = -2.0
-At[2,1] = 0.0
-@test A == At
-
-##
-
-
-end
-
 ##
 
 # abstract Nonparametric <: Function
 # This is what the internmediate user would be contributing
 mutable struct Pose1Pose1Test{T} <: AbstractRelativeRoots
   Dx::T
-  Pose1Pose1Test{T}() where T = new()
-  Pose1Pose1Test{T}(a::T) where T = new(a)
+  # Pose1Pose1Test{T}() where T = new()
+  # Pose1Pose1Test{T}(a::T) where T = new(a)
   # Pose1Pose1Test(a::T) where T = new(a)
 end
-Pose1Pose1Test(a::T) where T = Pose1Pose1Test{T}(a)
+# Pose1Pose1Test(a::T) where T = Pose1Pose1Test{T}(a)
 
 getSample(cf::CalcFactor{<:Pose1Pose1Test}, N::Int=1) = (reshape(rand(cf.factor.Dx,N),1,N),)
 
@@ -159,62 +92,6 @@ end
 
 ##
 
-@testset "Test in factor graph setting..." begin
-
-##
-
-N = 100
-p1 = rand(1,N)
-p2 = rand(1,N)
-t = Array{Array{Float64,2},1}()
-push!(t,p1)
-push!(t,p2)
-
-odo = Pose1Pose1Test(Normal(100.0,1.0))
-
-fg = initfg()
-X0 = addVariable!(fg, :x0, ContinuousEuclid{1})
-X1 = addVariable!(fg, :x1, ContinuousEuclid{1})
-addFactor!(fg, [:x0;:x1], odo, graphinit=false)
-fmd = FactorMetadata([X0;X1], [:x0; :x1], t, :null, nothing)
-
-ccw = CommonConvWrapper(odo, t[1], 1, t, fmd, measurement=getSample(odo, N))
-
-freshSamples!(ccw, N, fmd)
-x, res = zeros(1), zeros(1)
-
-@time for n in 1:N
-  ccw.cpt[Threads.threadid()].particleidx = n
-
-  ccw(res, x)
-  # each point should be near 100.0
-  @test 50.0 < res[1] < 150.0
-end
-
-# which variable position to solve for
-ccw.varidx = 2
-
-# do for a single point (i=1)
-
-r = nlsolve( ccw, [0.0], inplace=true )
-
-
-# for all points
-@time for i in 1:N
-  ccw.cpt[Threads.threadid()].particleidx = i
-    # ccw(x, res)
-  r = nlsolve( ccw, ccw.params[ccw.varidx][:,ccw.cpt[Threads.threadid()].particleidx], inplace=true )
-  ccw.params[ccw.varidx][1,ccw.cpt[Threads.threadid()].particleidx] = r.zero[1]
-end
-
-@test abs(Statistics.mean(p1)-0.0) < 4.0
-@test abs(Statistics.mean(p2)-100.0) < 4.0
-
-##
-
-end
-
-
 
 @testset "Test with CommonConvWrapper for un-permuted root finding..." begin
 
@@ -228,16 +105,7 @@ push!(t,p1)
 push!(t,p2)
 
 odo = Pose1Pose1Test(Normal(100.0,1.0))
-# varidx=2 means we are solving for p2 relative to p1
 
-# tfg_ = initfg()
-# addVariable!(tfg_, :x0, ContinuousScalar)
-# addFactor!(tfg_, [:x0], odo)
-
-# measurement = freshSamples(tfg_, :x0, N)
-# @show zDim = size(measurement[1],1)
-
-# solvefor = 2
 
 fg = initfg()
 X0 = addVariable!(fg, :x0, ContinuousEuclid{1})
@@ -245,9 +113,6 @@ initManual!(fg, :x0, zeros(1,100))
 X1 = addVariable!(fg, :x1, ContinuousEuclid{1})
 addFactor!(fg, [:x0;:x1], odo, graphinit=false)
 
-# fmd = FactorMetadata([X0;X1], [:x0; :x1], t, :null, nothing)
-# ccw = CommonConvWrapper(odo, t[solvefor], zDim, t, fmd, measurement=measurement)
-# @show ccw.varidx = solvefor
 
 pts = approxConv(fg, getFactor(fg, :x0x1f1), :x1)
 
@@ -269,18 +134,17 @@ pts = approxConv(fg, getFactor(fg, :x0x1f1), :x0)
 @test -10.0 < Statistics.mean(ccw.params[1]) < 10.0
 @test 90.0 < Statistics.mean(ccw.params[2]) < 110.0
 
-# ##
+##
 
 end
-
-
 
 
 # use the range only example, should give a circle with nothing in the middle
 
 
-
 @testset "Generic convolution testing in factor graph context..." begin
+
+##
 
 N=100
 p1 = randn(1,N)
@@ -324,5 +188,6 @@ pts = getVal(getVariable(fg,:x1))
 pts = getVal(getVariable(fg,:x2))
 @test abs(Statistics.mean(pts)-100.0) < 10.0
 
+##
 
 end
