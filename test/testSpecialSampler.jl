45375a25c5b5db1f03f1cdb2b1f861d11e217f27
dehann <dehann.fourie@gmail.com>
2021-03-18 17:50:31 -0400

    rm deprecations pre v0.22, and AMP compat v0.3

--- test/testSpecialSampler.jl
+++ test/testSpecialSampler.jl
@@ -4,33 +4,34 @@ using Test
 using IncrementalInference
 using DistributedFactorGraphs
 
-# import IncrementalInference: getSample
+import IncrementalInference: getSample
 
 
-struct SpecialPrior{T} <: AbstractPrior where T <: Distribution
+struct SpecialPrior{T <: SamplableBelief} <: AbstractPrior
   z::T
-  specialSampler::Function
+  # specialSampler::Function
 end
-specialSample(s::SpecialPrior, N::Int, fmd::FactorMetadata, Xi) = (reshape(rand(s.z,N),1,:), )
-SpecialPrior(z::T) where {T <: SamplableBelief}= SpecialPrior{T}(z, specialSample)
+getSample(s::CalcFactor{<:SpecialPrior}, N::Int) = (reshape(rand(s.factor.z,N),1,:), )
+
+# SpecialPrior(z::T) where {T <: SamplableBelief}= SpecialPrior{T}(z, specialSample)
 
 struct SpecialLinearOffset{T <: SamplableBelief} <: AbstractRelativeRoots
   z::T
-  specialSampler::Function
+  # specialSampler::Function
 end
 
-specialSample(s::SpecialLinearOffset, N::Int, fmd::FactorMetadata, Xi, Xj) = (reshape(rand(s.z,N),1,:), )
+function getSample(s::CalcFactor{<:SpecialLinearOffset}, N::Int)
+  fmd = s.metadata
+  (reshape(rand(s.factor.z,N),1,:), )
+end
 
-SpecialLinearOffset(z::T) where {T <: SamplableBelief} = SpecialLinearOffset{T}(z, specialSample)
+# SpecialLinearOffset(z::T) where {T <: SamplableBelief} = SpecialLinearOffset{T}(z, specialSample)
 
-function (s::SpecialLinearOffset)(res::Array{Float64},
-                                  userdata::FactorMetadata,
-                                  idx::Int,
-                                  meas::Tuple{Array{Float64, 2}},
-                                  X1::Array{Float64,2},
-                                  X2::Array{Float64,2}  )
+function (s::CalcFactor{<:SpecialLinearOffset})(meas,
+                                                x1,
+                                                x2  )
   #
-  res[1] = meas[1][idx] - (X2[1,idx] - X1[1,idx])
+  meas .- (x2 .- x1)
 end
 
 
