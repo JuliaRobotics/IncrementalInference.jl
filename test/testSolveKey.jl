7d2bc124feea6c68011247188c2f8c7c24f79a9d
dehann <dehann.fourie@gmail.com>
2021-04-24 21:38:37 -0400

    update testSolveKey

--- test/testSolveKey.jl
+++ test/testSolveKey.jl
@@ -5,6 +5,76 @@ using IncrementalInference
 
 ##
 
+@testset "Basic test of belief prediction on alternate solveKey" begin
+
+##
+
+
+fg = initfg()
+
+addVariable!(fg, :a, ContinuousScalar)
+addVariable!(fg, :b, ContinuousScalar)
+
+addFactor!(fg, [:a], Prior(Normal(10,1)), graphinit=false)
+addFactor!(fg, [:a;:b], LinearRelative(Normal(10,1)), graphinit=false)
+
+
+deleteVariableSolverData!(fg, :a, :default)
+deleteVariableSolverData!(fg, :b, :default)
+
+##
+
+pts, = sampleFactor(fg, :af1, 100)
+
+IIF.setDefaultNodeData!(getVariable(fg, :a), 0, 100, 1, solveKey=:testSolveKey, 
+                        initialized=false, varType=ContinuousScalar(), dontmargin=false)
+#
+
+initManual!(fg, :a, pts, :testSolveKey)
+
+@test !isInitialized(fg, :a)
+@test isInitialized(fg, :a, :testSolveKey)
+
+##
+
+IIF.setDefaultNodeData!(getVariable(fg, :b), 0, 100, 1, solveKey=:testSolveKey, 
+                        initialized=false, varType=ContinuousScalar(), dontmargin=false)
+#
+
+
+@test !(:default in listSolveKeys(getVariable(fg, :a)))
+@test !(:default in listSolveKeys(getVariable(fg, :b)))
+
+@test (:testSolveKey in listSolveKeys(getVariable(fg, :a)))
+@test (:testSolveKey in listSolveKeys(getVariable(fg, :b)))
+
+
+##
+
+doautoinit!(fg, :b, solveKey=:testSolveKey)
+
+
+##
+
+
+@test !(:default in listSolveKeys(getVariable(fg, :a)))
+@test !(:default in listSolveKeys(getVariable(fg, :b)))
+
+@test (:testSolveKey in listSolveKeys(getVariable(fg, :a)))
+@test (:testSolveKey in listSolveKeys(getVariable(fg, :b)))
+
+
+##
+
+@test isapprox( calcPPE(fg, :a, solveKey=:testSolveKey).suggested[1], 10, atol=1)
+@test isapprox( calcPPE(fg, :b, solveKey=:testSolveKey).suggested[1], 20, atol=1)
+
+
+##
+
+end
+
+
 @testset "test solve with unique solveKey, see #1219" begin
 
 ##
@@ -24,42 +94,31 @@ addFactor!(fg, [:b;:c], LinearRelative(Normal(10, 1)))
 addFactor!(fg, [:c;:d], LinearRelative(Normal(10, 1)))
 addFactor!(fg, [:d;:e], LinearRelative(Normal(10, 1)))
 
+getSolverParams(fg).graphinit=true
+
 
 ##
 
-# deleteVariableSolverData!.(fg, ls(fg))
-# @test listSolveKeys(fg) |> length == 0
+# getSolverParams(fg).limititers=30
+solveTree!(fg, solveKey=:testSolveKey )
 
 ##
 
-getSolverParams(fg).dbg=true
-# error("remember not 15")
-getSolverParams(fg).limititers=30
+@test isapprox( calcPPE(fg, :a, solveKey=:testSolveKey).suggested[1], 0, atol=2)
+@test isapprox( calcPPE(fg, :b, solveKey=:testSolveKey).suggested[1], 10, atol=2)
+@test isapprox( calcPPE(fg, :c, solveKey=:testSolveKey).suggested[1], 20, atol=2)
+@test isapprox( calcPPE(fg, :d, solveKey=:testSolveKey).suggested[1], 30, atol=2)
+@test isapprox( calcPPE(fg, :e, solveKey=:testSolveKey).suggested[1], 40, atol=2)
 
-# tree = buildTreeReset!(fg)
 
+##
+
+# using RoMEPlotting
+# Gadfly.set_default_plot_size(35cm,25cm)
 
 # ##
 
-# stuff = solveCliqUp!(fg, tree, 2, solveKey=:testSolveKey)
-# stuff = solveCliqUp!(fg, tree, 3, solveKey=:testSolveKey)
-# stuff = solveCliqUp!(fg, tree, 1, solveKey=:testSolveKey)
-
-
-##
-
-smtasks = Task[]
-solveTree!(fg, smtasks=smtasks, verbose=true, recordcliqs=ls(fg)  , solveKey=:testSolveKey )
-hists = fetchCliqHistoryAll!(smtasks)
-
-##
-
-using RoMEPlotting
-Gadfly.set_default_plot_size(35cm,25cm)
-
-##
-
-plotSLAM2D(fg, solveKey=:testSolveKey)
+# plotKDE(fg, ls(fg), solveKey=:testSolveKey)
 
 ##
 
