6f1d9491cadd56e3b12ebba71ba2d3e74745ea7c
Johannes Terblanche <Affie@users.noreply.github.com>
2021-07-05 18:15:55 +0200

    include test that gives dimention error

--- test/testSphereMani.jl
+++ test/testSphereMani.jl
@@ -1,5 +1,7 @@
 using DistributedFactorGraphs
 using IncrementalInference
+using Manifolds
+using StaticArrays
 using Test
 
 @testset "Test Sphere(2) prior" begin
@@ -29,4 +31,16 @@ doautoinit!(fg, :x0)
 vnd = getVariableSolverData(fg, :x0)
 @test all(isapprox.(mean(vnd.val), [1,0,0], atol=0.1))
 @test all(is_point.(Ref(M), vnd.val))
+
+v1 = addVariable!(fg, :x1, Sphere2)
+mf = ManifoldFactor(Sphere(2), MvNormal([0.1, 0.2], [0.05,0.05]))
+f = addFactor!(fg, [:x0, :x1], mf)
+
+##
+# Debugging Sphere error
+# DimensionMismatch("tried to assign 200 elements to 300 destinations")
+smtasks = Task[]
+solveTree!(fg; smtasks, verbose=true, recordcliqs=ls(fg))
+hists = fetchCliqHistoryAll!(smtasks);
+
 end
