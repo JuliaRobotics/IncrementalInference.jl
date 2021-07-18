69894728859127705a6e5ba15c5e021280b7736b
dehann <dehann.fourie@gmail.com>
2021-07-18 04:12:43 -0400

    wip

--- examples/dev/factor_gradients.jl
+++ examples/dev/factor_gradients.jl
@@ -20,41 +20,48 @@ function _prepFactorGradientLambdas(fct::Union{<:AbstractRelativeMinimize,<:Abst
                                     T_pt_args...;
                                     h::Real=1e-8  ) # numerical diff perturbation size
   #
-  
+  # get manifolds for all variables
+  M = (x->getManifold(x[1])).(T_pt_args)
+  # use the temporary factor graph throughout
+  tfg = initfg()
+
   # gradients relative to coords requires 
-  slack_resid = calcFactorResidualTemporary(fct, measurement, T_pt_args...)
-  coord_s = IIF._evalFactorTemporary!(fct, measurement, T_pt_args..., _slack=slack_resid )
+  slack_resid = calcFactorResidualTemporary(fct, measurement, T_pt_args..., tfg=tfg)
+  # coord_s = IIF._evalFactorTemporary!(fct, sfidx, measurement, T_pt_args..., tfg=tfg, newFactor=false, _slack=slack_resid )
   
+  # perturb the coords of one variable on the factor, and return a new vector of TypePoint tuples
+  # TODO, replace with retract operations instead
+  coord_ =        (s) -> AMP.makeCoordsFromPoint(M[s], T_pt_args[s][2])
+  coord_h =       (s,i, crd=coord_(s)) -> (crd[i] += h; crd)
+  # reassemble TypePoint vector with perturbation at (s,i)
+  T_pth_s_i =     (s,i) -> (T_pt_args[s][1], AMP.makePointFromCoords(M[s], coord_h(s,i), T_pt_args[s][2]) )
+  arr_T_pth_s_i = (s,i) -> ((T_pt_args[1:(s-1)])..., T_pth_s_i(s,i), (T_pt_args[(s+1):end])...)
   # build a residual calculation specifically considering graph factor selections `s`, e.g. for binary `s ∈ {1,2}``.
-  δ_s = (s,T_pt) -> testFactorResidualBinary(fct, measurement, (T_pt_args[1:(s-1)])..., T_pt, (T_pt_args[(s+1):end])...)
+  f_sih =         (s,i) -> calcFactorResidualTemporary(fct, measurement, arr_T_pth_s_i(s,i)..., tfg=tfg, newFactor=false, _slack=slack_resid )
+  # standard calculus derivative definition (in coordinate space)
+  Δf_si =         (s,i, crd=coord_(s)) -> (f_sih(s,i) - crd)./h
+
   # TODO generalize beyond binary
   λ_fncs = []    # by factor's variable order
   λ_sizes = []   # by factor's variable order
 
   # each variable T, go down to coords, add eps to a coord, back to point and look at the change in residual (assumed in coords for AbstractRelative[Minimize/Roots])
   # TODO change `a_` to `s_` as variable selection by factor order
-  for (a_, Tpt_) in enumerate(T_pt_args)
+  for (s_, Tpt_) in enumerate(T_pt_args)
     # assume binary factor to start, these are variables A and B
     Ta = Tpt_[1]
-    Ma = getManifold(Ta)
-    pt_a = Tpt_[2]
-    u_a = pt_a
+    pt_s = Tpt_[2]
       
-    # replace with retract operations only
-    coords_a = AMP.makeCoordsFromPoint(Ma, pt_a)
-    push!(λ_sizes, length(coords_a))
-    # perturb coords
-    pr_coord_ = (i) -> (ca_ = deepcopy(coords_a); ca_[i] += h; ca_)  
-    pt_a_i = (i) -> AMP.makePointFromCoords(Ma,pr_coord_(i),u_a)
-    resid_a_i = (s,i) -> δ_s(s, (Ta, pt_a_i(i)))
-    # standard calculus derivative definition (in coordinate space)
-    Δf_h = (i) -> (resid_a_i(a_,i) - resid_)./h
-    # jacobian block in reverse indices
-    ▽f = () -> ((i->Δf_h(i)).(1:length(coords_a)))
+    crd_s = coord_(s_)
+    len_s = length(crd_s)
+    push!(λ_sizes, len_s)
+
+    # jacobian block per s, for each i
+    ▽f_s = (s, crd=coord_(s)) -> ((i->Δf_si(s,i,crd)).(1:length(crd)))
     # jacobian stored in user provided matrix
-    ▽f_ij! = (J::AbstractMatrix{<:Real}) -> (J_ = ▽f(); (@cast J[i,j] = J_[j][i]))
+    ▽f_s_J! = (J::AbstractMatrix{<:Real}, s) -> (J_ = ▽f_s(s); (@cast J[i,s] = J_[j][s]))
     # function is ready but calculation for actual jacobian values must still be done
-    push!(λ_fncs, ▽f_ij!)
+    push!(λ_fncs, ▽f_J!)
   end
   
   # build Tuple{Tuple{<:Function,size}...}
