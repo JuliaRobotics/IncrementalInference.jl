e53af36bd307f11252d07fdf3ca85d37d4699219
dehann <dehann.fourie@gmail.com>
2021-09-13 15:58:11 -0400

    some cleanup

--- src/GraphProductOperations.jl
+++ src/GraphProductOperations.jl
@@ -102,17 +102,11 @@ function localProduct(dfg::AbstractDFG,
   # vector of all neighbors as Symbols
   lb = getNeighbors(dfg, sym)
 
-  # store proposal beliefs
-  dens = Array{ManifoldKernelDensity,1}()
+  # store proposal beliefs, TODO replace Abstract with concrete type
+  dens = Vector{ManifoldKernelDensity}()
   
   mkd, sinfd = propagateBelief(dfg, getVariable(dfg, sym), map(x->getFactor(dfg, x), lb); solveKey=solveKey, logger=logger, dens=dens, N=N )
-
-  # # partials = Dict{Any, Vector{ManifoldKernelDensity}}()
-  # pGM, sinfd = predictbelief(dfg, sym, lb, solveKey=solveKey, logger=logger, dens=dens, N=N )
-  # # make manifold belief from product
-  # vari = getVariable(dfg, sym)
-  # mkd = AMP.manikde!(getManifold(getVariableType(vari)), pGM )
-
+  
   return mkd, dens, lb, sinfd
 end
 localProduct(dfg::AbstractDFG, lbl::AbstractString; kw...) = localProduct(dfg, Symbol(lbl); kw...)
