769379470dcda773780e66ccaabc45c7fe9be5c4
dehann <dehann.fourie@gmail.com>
2021-10-09 17:42:15 -0400

    adj test slightly

--- test/testEuclidDistance.jl
+++ test/testEuclidDistance.jl
@@ -124,11 +124,11 @@ for i in 1:1
   L1_ = getBelief(fg, :l1) |> getPoints
   @cast L1[i,j] := L1_[j][i] 
   # check that two modes exist
-  @test (0.08*N < sum(-50 .< L1[1,:] .< 50))
-  @test (0.08*N < sum(-50 .< L1[2,:] .< 50))
+  @test (0.05*N < sum(-50 .< L1[1,:] .< 50))
+  @test (0.05*N < sum(-50 .< L1[2,:] .< 50))
   # @error "suppressing dual mode tests, MUST restore before IIF v0.25, see #1305"
-  @test (0.08*N < sum(50 .< L1[1,:] .< 150)) # always this one
-  @test (0.08*N < sum(50 .< L1[2,:] .< 150))
+  @test (0.05*N < sum(50 .< L1[1,:] .< 150)) # always this one
+  @test (0.05*N < sum(50 .< L1[2,:] .< 150))
 end
 
 # at least one of the 3 solves should produce the right result
