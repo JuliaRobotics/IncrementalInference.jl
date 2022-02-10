122c1b286e7d1c813f71b75a8404fd227b9dcddf
Johannes Terblanche <Affie@users.noreply.github.com>
2022-02-10 18:11:49 +0200

    Remove inner constructor PackedGenericMarginal

--- src/Factors/GenericMarginal.jl
+++ src/Factors/GenericMarginal.jl
@@ -18,8 +18,6 @@ Base.@kwdef mutable struct PackedGenericMarginal <: AbstractPackedFactor
     Zij::Array{Float64,1}
     Cov::Array{Float64,1}
     W::Array{Float64,1}
-    PackedGenericMarginal() = new()
-    PackedGenericMarginal(a,b,c) = new(a,b,c)
 end
 function convert(::Type{PackedGenericMarginal}, d::GenericMarginal)
   return PackedGenericMarginal(d.Zij, d.Cov, d.W)
