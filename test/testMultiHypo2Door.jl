f5cf07a9dced0f4047c79f74ec6a29581feb7c06
dehann <dehann.fourie@gmail.com>
2022-04-23 15:14:14 -0700

    fix classic 3 door test

--- test/testMultiHypo2Door.jl
+++ test/testMultiHypo2Door.jl
@@ -13,7 +13,8 @@ n_samples = 200
 # initialize mean landmark locations
 l0 = 0.0
 l1 = 10.0
-l2 = 40.0
+l2 = 20.0
+l3 = 40.0
 
 # "Ground-truth" robot poses
 x0 = 0.0
@@ -23,11 +24,13 @@ x3 = 40.0
 
 ##
 
-@testset "2door basic binary multihypothesis tests..." begin
+@testset "Basic 3 door, trinary multihypothesis tests..." begin
 
 ## Initialize empty factor graph
 fg = initfg()
-fg.solverParams.N = n_samples
+getSolverParams(fg).N = n_samples
+getSolverParams(fg).gibbsIters = 5
+
 # Place strong prior on locations of three "doors"
 addVariable!(fg, :l0, ContinuousScalar, N=n_samples)
 addFactor!(fg, [:l0], Prior(Normal(l0, lm_prior_noise)))
@@ -35,14 +38,18 @@ addFactor!(fg, [:l0], Prior(Normal(l0, lm_prior_noise)))
 addVariable!(fg, :l1, ContinuousScalar, N=n_samples)
 addFactor!(fg, [:l1], Prior(Normal(l1, lm_prior_noise)))
 
+addVariable!(fg, :l2, ContinuousScalar, N=n_samples)
+addFactor!(fg, [:l2], Prior(Normal(l2, lm_prior_noise)))
+
+addVariable!(fg, :l3, ContinuousScalar, N=n_samples)
+addFactor!(fg, [:l3], Prior(Normal(l3, lm_prior_noise)))
+
 
 # Add first pose
 addVariable!(fg, :x0, ContinuousScalar, N=n_samples)
 
 # Make first "door" measurement
-# addFactor!(fg, [:x0; :l0], LinearRelative(Normal(0, meas_noise)))
-addFactor!(fg, [:x0; :l0; :l1], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; 1.0/2.0; 1.0/2.0])
-
+addFactor!(fg, [:x0; :l0; :l1; :l2; :l3], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; (1/4 for _=1:4)...])
 
 # Add second pose
 addVariable!(fg, :x1, ContinuousScalar, N=n_samples)
@@ -51,8 +58,42 @@ addVariable!(fg, :x1, ContinuousScalar, N=n_samples)
 addFactor!(fg, [:x0; :x1], LinearRelative(Normal(x1-x0, odom_noise)))
 
 # Make second "door" measurement
-# addFactor!(fg, [:x1; :l1], LinearRelative(Normal(0, meas_noise)) )
-addFactor!(fg, [:x1; :l0; :l1], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; 1.0/2.0; 1.0/2.0])
+addFactor!(fg, [:x1; :l0; :l1; :l2; :l3], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; (1/4 for _=1:4)...])
+
+##
+
+solveGraph!(fg)
+
+##
+
+# check there is enough likelihood in the right places
+@test 0.1 < getBelief(fg, :x0)([l0])[1]
+@test 0.1 < getBelief(fg, :x0)([l1])[1]
+@test getBelief(fg, :x0)([l2])[1] < 0.3
+
+@test getBelief(fg, :x1)([l0])[1] < 0.3
+@test 0.1 < getBelief(fg, :x1)([l1])[1]
+@test 0.1 < getBelief(fg, :x1)([l2])[1]
+
+
+##
+
+for i in 1:3
+  solveGraph!(fg);
+end
+
+##
+
+
+# check there is enough likelihood in the right places
+@test 0.1 < getBelief(fg, :x0)([l0])[1]
+@test 0.1 < getBelief(fg, :x0)([l1])[1]
+@test getBelief(fg, :x0)([l2])[1] < 0.03
+
+@test getBelief(fg, :x1)([l0])[1] < 0.03
+@test 0.1 < getBelief(fg, :x1)([l1])[1]
+@test 0.1 < getBelief(fg, :x1)([l2])[1]
+
 
 ## Add one more pose/odometry to invoke issue #236
 
@@ -66,96 +107,74 @@ tree = solveTree!(fg)
 # drawGraph(fg)
 # drawTree(tree, show=true)
 
-@test abs(getKDEMean(getBelief(fg, :x0))[1]-x0) < 2.0
-@test abs(getKDEMean(getBelief(fg, :x1))[1]-x1) < 2.0
-@test abs(getKDEMean(getBelief(fg, :x2))[1]-x2) < 2.0
+##
 
-@test abs(getKDEMean(getBelief(fg, :l0))[1]-l0) < 3.0
-@test abs(getKDEMean(getBelief(fg, :l1))[1]-l1) < 3.0
+# check there is enough likelihood in the right places
+@test 0.1 < getBelief(fg, :x0)([l0])[1]
+@test 0.1 < getBelief(fg, :x0)([l1])[1]
+@test getBelief(fg, :x0)([l2])[1] < 0.03
+
+@test getBelief(fg, :x1)([l0])[1] < 0.03
+@test 0.1 < getBelief(fg, :x1)([l1])[1]
+@test 0.1 < getBelief(fg, :x1)([l2])[1]
+
+dx = x2-x1
+@test getBelief(fg, :x2)([l0 + dx])[1] < 0.03
+@test 0.1 < getBelief(fg, :x2)([l1 + dx])[1]
+@test 0.1 < getBelief(fg, :x2)([l2 + dx])[1]
 
 ##
 
-
-end
-
-#
-# using RoMEPlotting
-# Gadfly.set_default_plot_size(35cm,25cm)
-#
-# plotKDE(fg, [:l0;:l1])
-# plotKDE(fg, [:x0;:x1;:x2])
-
-
-@testset "3door basic trinary multihypo test..." begin
-
-##
-
-# Initialize empty factor graph
-fg = initfg()
-
-# Place strong prior on locations of three "doors"
-addVariable!(fg, :l0, ContinuousScalar, N=n_samples)
-addFactor!(fg, [:l0], IIF.Prior(Normal(l0, lm_prior_noise)))
-
-addVariable!(fg, :l1, ContinuousScalar, N=n_samples)
-addFactor!(fg, [:l1], IIF.Prior(Normal(l1, lm_prior_noise)))
-
-addVariable!(fg, :l2, ContinuousScalar, N=n_samples)
-addFactor!(fg, [:l2], IIF.Prior(Normal(l2, lm_prior_noise)))
-
-# Add first pose
-addVariable!(fg, :x0, ContinuousScalar, N=n_samples)
-
-# Make first "door" measurement
-addFactor!(fg, [:x0; :l0; :l1; :l2], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; 1.0/3.0; 1.0/3.0; 1.0/3.0])
-
-# Add second pose
-addVariable!(fg, :x1, ContinuousScalar, N=n_samples)
-
-# # Gaussian transition model
-addFactor!(fg, [:x0; :x1], LinearRelative(Normal(x1-x0, odom_noise)))
-
-# # Make second "door" measurement
-addFactor!(fg, [:x1; :l0; :l1; :l2], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; 1.0/3.0; 1.0/3.0; 1.0/3.0])
-
-# # Add third pose
-addVariable!(fg, :x2, ContinuousScalar, N=n_samples)
-
-# # Gaussian transition model
-addFactor!(fg, [:x1; :x2], LinearRelative(Normal(x2-x1, odom_noise)))
-
-# Add fourth pose
+# Add third pose
 addVariable!(fg, :x3, ContinuousScalar, N=n_samples)
-
-# Add odometry transition and new landmark sighting
-addFactor!(fg, [:x2, :x3], LinearRelative(Normal(x3-x2, odom_noise)))
-addFactor!(fg, [:x3; :l0; :l1; :l2], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; 1.0/3.0; 1.0/3.0; 1.0/3.0])
+addFactor!(fg, [:x2; :x3], LinearRelative(Normal(x3-x2, odom_noise)))
 
 
-initAll!(fg)
-
-# drawGraph(fg)
-solveTree!(fg)
-
-
-@test abs(getKDEMean(getBelief(fg, :x0))[1]-x0) < 2.0
-@test abs(getKDEMean(getBelief(fg, :x1))[1]-x1) < 2.0
-@test abs(getKDEMean(getBelief(fg, :x2))[1]-x2) < 2.0
-
-@test abs(getKDEMean(getBelief(fg, :l0))[1]-l0) < 3.0
-@test abs(getKDEMean(getBelief(fg, :l1))[1]-l1) < 3.0
-@test abs(getKDEMean(getBelief(fg, :l2))[1]-l2) < 3.0
+# Make third "door" measurement
+addFactor!(fg, [:x3; :l0; :l1; :l2; :l3], LinearRelative(Normal(0, meas_noise)), multihypo=[1.0; (1/4 for _=1:4)...])
 
 ##
 
+solveGraph!(fg)
+
+##
+
+@test isapprox(mean(getBelief(fg, :x0))[1], x0; atol = 2.0)
+@test isapprox(mean(getBelief(fg, :x1))[1], x1; atol = 2.0)
+@test isapprox(mean(getBelief(fg, :x2))[1], x2; atol = 2.0)
+@test isapprox(mean(getBelief(fg, :x3))[1], x3; atol = 2.0)
+
+@test isapprox(mean(getBelief(fg, :l0))[1], l0; atol = 3.0)
+@test isapprox(mean(getBelief(fg, :l1))[1], l1; atol = 3.0)
+@test isapprox(mean(getBelief(fg, :l2))[1], l2; atol = 3.0)
+@test isapprox(mean(getBelief(fg, :l3))[1], l3; atol = 3.0)
+
+##
+
+# check the PPEs are the same
+@test isapprox(getPPE(fg, :x0).suggested[1], x0; atol = 2.0)
+@test isapprox(getPPE(fg, :x1).suggested[1], x1; atol = 2.0)
+@test isapprox(getPPE(fg, :x2).suggested[1], x2; atol = 2.0)
+@test isapprox(getPPE(fg, :x3).suggested[1], x3; atol = 2.0)
+
+@test isapprox(getPPE(fg, :l0).suggested[1], l0; atol = 3.0)
+@test isapprox(getPPE(fg, :l1).suggested[1], l1; atol = 3.0)
+@test isapprox(getPPE(fg, :l2).suggested[1], l2; atol = 3.0)
+@test isapprox(getPPE(fg, :l3).suggested[1], l3; atol = 3.0)
+
+
+
+##
+
+
 end
+
 
 
 # using RoMEPlotting
 # Gadfly.set_default_plot_size(35cm,25cm)
 #
-# plotKDE(fg, [:l0;:l1;:l2])
-# plotKDE(fg, [:x0;:x1;:x2])
+# plotBelief(fg, sortDFG(ls(fg, r"x")))
 
 
 #
