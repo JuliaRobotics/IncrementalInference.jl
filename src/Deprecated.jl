db6d58a519e24c580cc57a076ff0377cb3b8b735
Johannes Terblanche <Affie@users.noreply.github.com>
2022-07-10 21:33:25 +0200

    remove comments in Deprecated.jl

--- src/Deprecated.jl
+++ src/Deprecated.jl
@@ -32,33 +32,4 @@ end
 ## Deprecate code below before v0.31
 ##==============================================================================
 
-
 @deprecate initManual!(w...;kw...) initVariable!(w...;kw...)
-
-
-
-##==============================================================================
-## Deprecate code below before v0.29
-##==============================================================================
-
-# function manikde!(vartype::Union{InstanceType{<:InferenceVariable}, InstanceType{<:AbstractFactor}},
-#                   pts::AbstractVector{P} ) where {P <: Union{<:AbstractArray,<:Number,<:ProductRepr,<:Manifolds.ArrayPartition} }
-#   #
-#   M = getManifold(vartype)
-#   infoPerCoord=ones(manifold_dimension(M))
-#   return AMP.manikde!(M, pts; infoPerCoord)
-# end
-
-# @deprecate manikde!(varType::Union{InstanceType{<:InferenceVariable}, InstanceType{<:AbstractFactor}}, pts::AbstractVector{P}, bw::Vector{<:Real} ) where {P <: Union{<:AbstractArray,<:Number,<:ProductRepr,<:Manifolds.ArrayPartition} } manikde!(varType, pts; bw)
-
-# @deprecate kde!(em::TreeBelief) manikde!(em)
-
-# # DFG v0.18/19
-# export FunctorInferenceType, PackedInferenceType
-
-# @deprecate _evalType(pt::String) DFG.getTypeFromSerializationModule(pt)
-
-# # LightDFG will be replaced by GraphsDFG
-# export LightDFG
-# export InMemDFGType
-# const InMemDFGType = DFG.LocalDFG{SolverParams}
