a171c90ab7cfff16ec441968bf5460a7fd4781e7
Johannes Terblanche <Affie@users.noreply.github.com>
2023-11-03 08:20:22 +0200

    wip parametric tree solve

--- IncrementalInference/src/CliqueStateMachine/services/CliqueStateMachine.jl
+++ IncrementalInference/src/CliqueStateMachine/services/CliqueStateMachine.jl
@@ -956,7 +956,7 @@ function updateFromSubgraph_StateMachine(csmc::CliqStateMachineContainer)
   logCSM(
     csmc,
     "CSM-5 Clique $(csmc.cliq.id) finished, solveKey=$(csmc.solveKey)";
-    loglevel = Logging.Info,
+    loglevel = Logging.Debug,
   )
   return IncrementalInference.exitStateMachine
 end
