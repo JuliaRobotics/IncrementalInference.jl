a45c1a9b67b6ef3e14b180737af7a01e2bfd6a07
dehann <dehann.fourie@gmail.com>
2026-06-09 00:35:54 -0700

    workaround HoDe TreeBelief, see #1929

--- IncrementalInference/src/entities/BeliefTypes.jl
+++ IncrementalInference/src/entities/BeliefTypes.jl
@@ -92,7 +92,13 @@ end
 
 function TreeBelief(state::State, solvDim::Real = 0)
   pts = getPoints(state.belief; permute=false) # TODO likely want to go back to sorted order here, DX debugging with permute=false
-  cv = getBW(state.belief)[1] # FIXME, bw for nonparametric, cov for parametric -- can reuse bw for parametric during AMP 15 refactor
+  cv = if length(getBW(state.belief)) <= 0 || !isassigned(getBW(state.belief), 1)
+    @info "WHY" string(getBW(state.belief)) Npts(state.belief)
+    @warn "TreeBelief constructor: belief has no bandwidth, defaulting to identity, incorrect -- must refactor, see #1929" maxlog = 20
+    cov(state.belief)
+  else
+    getBW(state.belief)[1] # FIXME, bw for nonparametric, cov for parametric -- can reuse bw for parametric during AMP 15 refactor
+  end
   obsv = DFG.refObservability(state)
   statekind = getStateKind(state)
 
